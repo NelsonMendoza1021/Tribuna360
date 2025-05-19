7c893da7e8af7d341931198fdb73c857fc1d837e
NelsonMendoza1021 <nelsondavid1021@gmail.com>
2025-05-18 19:59:49 -0500

    inicialización del proyecto en SpringBoot

---
+++ Tribuna360/src/test/java/com/tribuna360/Tribuna360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tribuna360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tribuna360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
